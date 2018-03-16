6d61b0fe97d1f37d68f0f4756f60e05e2335788d
TheNessi <Macuta46@ya.ru>
2018-03-16 21:03:50 +0300

    fix formula and form

--- labwork/Form1.cs
+++ labwork/Form1.cs
@@ -22,39 +22,67 @@ namespace labwork
         {
             dataGridView1.Rows.Clear();
             Random Rand = new Random();
-            int n = 0;
+            int count_b = 0;
+            int count_exp = 0;
             double lam;
             double disp;
             double mean;
             double tmp;
             double mu;
+            double etta_tmp;
 
-            n = Convert.ToInt32(textBox1.Text);
+            count_b = Convert.ToInt32(textBox1.Text);
             mean = Convert.ToDouble(textBox2.Text);
             disp = Convert.ToDouble(textBox3.Text);
+            count_exp = Convert.ToInt32(textBox4.Text);
             lam = 1.0 / (Math.Sqrt(disp));
             mu = (1.0 / lam) - mean;
 
-            double[] etta = new double[n];
+            label5.Text = "lambda: " + Convert.ToString(lam);
+            label6.Text = "mu: " + Convert.ToString(mu);
 
-            for (int i = 0; i < n; i++)
+            if (mu >= 0)
             {
-                etta[i] = 0;
+                double[] etta = new double[count_exp];
+
+                for (int i = 0; i < count_exp; i++)
+                {
+                    etta[i] = 0;
+                }
+
+                for (int i = 0; i < count_exp; i++)
+                {
+                    for (int j = 0; j < count_b; j++)
+                    {
+                        tmp = Rand.NextDouble();
+                        etta_tmp = -((Math.Log(
+                                         Math.Exp(mu - lam * mu) - tmp)
+                                                    - mu) / lam);
+                        etta[i] += etta_tmp;
+                    }
+                }
+
+                Array.Sort(etta);
+
+                for (int i = 0; i < count_exp; i++)
+                {
+                    dataGridView1.Rows.Add();
+                    dataGridView1.Rows[i].Cells[0].Value = Convert.ToString(i);
+                    dataGridView1.Rows[i].Cells[1].Value = Convert.ToString(etta[i]);
+                }
             }
-
-            for (int i = 0; i < n; i++)
+            else
             {
-                tmp = Rand.NextDouble();
-                etta[i] = mu - (Math.Log(tmp) / lam) ;
-            }
+                var result = MessageBox.Show("Mu<0, попробуйте задать другое среднее или дисперсию.", "Ошибка", MessageBoxButtons.OK);
 
-            Array.Sort(etta);
+                if (result == System.Windows.Forms.DialogResult.Yes)
+                {
 
-            for (int i = 0; i < n; i++)
-            {
-                dataGridView1.Rows.Add();
-                dataGridView1.Rows[i].Cells[0].Value = Convert.ToString(i);
-                dataGridView1.Rows[i].Cells[1].Value = Convert.ToString(etta[i]);
+                    // Closes the parent form.
+
+                    this.Close();
+
+                }
             }
         }
     }
